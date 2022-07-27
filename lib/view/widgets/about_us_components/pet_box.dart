fdc088f06a09fc993f44d3da62cab269c0a7ba91
Hany Sameh <hanysameh@Hanys-MacBook-Pro.local>
2022-07-28 00:00:40 +0200

    Handle Hover

--- lib/view/widgets/about_us_components/pet_box.dart
+++ lib/view/widgets/about_us_components/pet_box.dart
@@ -2,42 +2,63 @@ import 'package:flutter/material.dart';
 
 import '../../../utilities/colors.dart';
 
-class PetBox extends StatelessWidget {
+class PetBox extends StatefulWidget {
   const PetBox({Key? key, required this.isCat}) : super(key: key);
   final bool isCat;
+
+  @override
+  State<PetBox> createState() => _PetBoxState();
+}
+
+class _PetBoxState extends State<PetBox> {
+  bool _isHover = false;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(15.0),
-      width: 105.0,
-      height: 105.0,
-      decoration: BoxDecoration(
-        color: isCat == true
-            ? AppColors.lightBrown
-            : AppColors.lightGrey.withOpacity(0.5),
-        borderRadius: BorderRadius.circular(10.0),
-        border: Border.all(
-          width: 2,
-          color: AppColors.darkBrown,
+    return MouseRegion(
+      onHover: (_) {
+        setState(() {
+          _isHover = true;
+        });
+      },
+      onExit: (_) {
+        setState(() {
+          _isHover = false;
+        });
+      },
+      child: Container(
+        padding: const EdgeInsets.all(15.0),
+        width: 105.0,
+        height: 105.0,
+        decoration: BoxDecoration(
+          color: _isHover == true
+              ? AppColors.lightBrown
+              : AppColors.lightGrey.withOpacity(0.5),
+          borderRadius: BorderRadius.circular(10.0),
+          border: Border.all(
+            width: 2,
+            color: AppColors.darkBrown,
+          ),
         ),
-      ),
-      child: Column(
-        children: [
-          Image.asset(
-            isCat == true ? 'assets/icons/cat.png' : 'assets/icons/dog.png',
-            width: 40.0,
-            height: 40.0,
-          ),
-          const SizedBox(
-            height: 15.0,
-          ),
-          Text(
-            isCat == true ? 'Cat' : 'Dog',
-            style: Theme.of(context).textTheme.bodyLarge!.copyWith(
-                  color: AppColors.darkBrown,
-                ),
-          ),
-        ],
+        child: Column(
+          children: [
+            Image.asset(
+              widget.isCat == true
+                  ? 'assets/icons/cat.png'
+                  : 'assets/icons/dog.png',
+              width: 40.0,
+              height: 40.0,
+            ),
+            const SizedBox(
+              height: 15.0,
+            ),
+            Text(
+              widget.isCat == true ? 'Cat' : 'Dog',
+              style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                    color: AppColors.darkBrown,
+                  ),
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/view/widgets/about_us_components/pets_needs_home_list.dart
+++ lib/view/widgets/about_us_components/pets_needs_home_list.dart
@@ -2,51 +2,79 @@ import 'package:flutter/material.dart';
 
 import '../../../utilities/colors.dart';
 
-class PetsNeedsHomeList extends StatelessWidget {
+class PetsNeedsHomeList extends StatefulWidget {
   const PetsNeedsHomeList({Key? key}) : super(key: key);
 
   @override
+  State<PetsNeedsHomeList> createState() => _PetsNeedsHomeListState();
+}
+
+class _PetsNeedsHomeListState extends State<PetsNeedsHomeList> {
+  bool _isHover = false;
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 400.0,
-      width: 250.0,
-      padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(15.0),
-        border: Border.all(
-          width: 2,
-          color: AppColors.brown,
+    return MouseRegion(
+      onHover: (_) {
+        setState(() {
+          _isHover = true;
+        });
+      },
+      onExit: (_) {
+        setState(() {
+          _isHover = false;
+        });
+      },
+      child: Container(
+        height: 400.0,
+        width: 250.0,
+        padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15.0),
+          border: Border.all(
+            width: 2,
+            color: AppColors.brown,
+          ),
+          color: AppColors.lightGrey.withOpacity(0.5),
+          boxShadow: _isHover == true
+              ? [
+                  BoxShadow(
+                    color: AppColors.black.withOpacity(0.3),
+                    blurRadius: 10.0,
+                    blurStyle: BlurStyle.normal,
+                    offset: const Offset(5, 10),
+                  ),
+                ]
+              : [],
         ),
-        color: AppColors.lightGrey.withOpacity(0.5),
-      ),
-      child: Column(
-        children: [
-          SizedBox(
-            width: 300.0,
-            height: 300.0,
-            child: Image.asset(
-              'assets/images/dog-nextto-cat.png',
+        child: Column(
+          children: [
+            SizedBox(
+              width: 300.0,
+              height: 300.0,
+              child: Image.asset(
+                'assets/images/dog-nextto-cat.png',
+              ),
             ),
-          ),
-          const SizedBox(
-            height: 20.0,
-          ),
-          Text(
-            'Dogs',
-            style: Theme.of(context).textTheme.subtitle1!.copyWith(
-                  color: AppColors.black,
-                ),
-          ),
-          const SizedBox(
-            height: 20.0,
-          ),
-          Text(
-            'petology',
-            style: Theme.of(context).textTheme.caption!.copyWith(
-                  color: AppColors.black,
-                ),
-          ),
-        ],
+            const SizedBox(
+              height: 20.0,
+            ),
+            Text(
+              'Dogs',
+              style: Theme.of(context).textTheme.subtitle1!.copyWith(
+                    color: AppColors.black,
+                  ),
+            ),
+            const SizedBox(
+              height: 20.0,
+            ),
+            Text(
+              'petology',
+              style: Theme.of(context).textTheme.caption!.copyWith(
+                    color: AppColors.black,
+                  ),
+            ),
+          ],
+        ),
       ),
     );
   }
